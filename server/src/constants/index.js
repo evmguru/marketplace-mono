524d10f10ce4aa6eebc2544fb51cbcc083177e2d
evmguru <block.d.co@gmail.com>
2022-11-05 04:31:26 -0500

    update server & config

--- server/src/constants/index.js
+++ server/src/constants/index.js
@@ -8,3 +8,7 @@ export const provider = new providers.JsonRpcProvider(envVars.web3NodeUrl)
 const openAiConfig = new Configuration({ apiKey: envVars.openAiApiKey })
 
 export const openai = new OpenAIApi(openAiConfig)
+
+export function makeStorageClient() {
+  return new Web3Storage({ token: envVars.web3StorageToken })
+}
--- server/src/validations/content.validation.js
+++ server/src/validations/content.validation.js
@@ -3,6 +3,7 @@ import Joi from 'joi'
 export const draw = {
   query: {
     phrase: Joi.string().required(),
+    owner: Joi.string().required(),
     burnIds: Joi.string().optional(),
   },
 }
