007d14f89ca93443ee27f44a9a3ffe89b151da82
evmguru <block.d.co@gmail.com>
2022-11-05 04:29:52 -0500

    more goerli create script

--- contract/scripts/goerli-create.ts
+++ contract/scripts/goerli-create.ts
@@ -1,5 +1,5 @@
 import { config as dotenvConfig } from 'dotenv'
-import { utils, providers, Wallet } from 'ethers'
+import { utils, providers, BigNumber, Wallet } from 'ethers'
 import { resolve } from 'path'
 
 import { Nordle, Nordle__factory } from '../types'
@@ -12,24 +12,31 @@ if (!mnemonic) {
   throw new Error('Please set your MNEMONIC in a .env file')
 }
 
-const provider = new providers.JsonRpcProvider('https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161')
+const provider = new providers.JsonRpcProvider('https://rpc.ankr.com/eth_goerli')
 const wallet = Wallet.fromMnemonic(mnemonic)
 const signer = wallet.connect(provider)
 
 // const contractAddress = '0x09aD4fe84C6C3c4C1F31297a751436Df9a506877'
-const contractAddress = '0xEeC7c9b7E2A84dD649236d73Cb15614F357bf916'
+const contractAddress = '0x701E42E843167AAE8658AB8F5ADcDb4BD2Ed6833'
 
 async function main() {
 	const nordle = Nordle__factory.connect(contractAddress, signer)
 
-	const tx = await nordle.requestCreateWord({ gasLimit: 5_000_000 })
+	// const tx = await nordle['requestCreateWord()']({ gasLimit: 5_000_000 })
+	// console.log(tx)
+	// console.log(await tx.wait())
+
+	const tx = await nordle['requestCreateWord(string)']('unicorn', { gasLimit: 5_000_000, value: BigNumber.from(5).mul(BigNumber.from(10).pow(16)) })
 	console.log(tx)
 	console.log(await tx.wait())
 
 	// const tx = await nordle.withdrawLink({ gasLimit: 5_000_000 })
 	// console.log(await tx.wait())
 
-	// console.log(await nordle.tokenWords(0))
+	// const tokenId = 0
+	// console.log(await nordle.tokenWords(tokenId))
+	// console.log(await nordle.ownerOf(tokenId))
+	// console.log(await nordle.tokenURI(tokenId))
 }
 
 main().catch((error) => {
